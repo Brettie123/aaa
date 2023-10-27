b487237fe04d0643f2dc824f963089d24d244975
Brett costanzo <127500925+Brettie123@users.noreply.github.com>
2023-10-27 12:00:04 -0300

    :)
    juego fondo

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+console.log ("main.js loaded")
+
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+let x = 0;
+let y = 0;
+
+function update (){
+ 
+requestAnimationFrame (update)
+}
+update()
